1c8e9276a4ee80b3344c293d8fc2e73b2281be62
Сергей Комольцев <sk@Sergejs-MacBook-Air.local>
2023-09-18 21:30:20 +0300

    Первый проект

---
+++ HelloWorld.py
@@ -0,0 +1,31 @@
+# print('Hello world!')
+
+# Программа 1
+# s = input('Как тебя зовут? ')
+# print(f'Привет, {s}!')
+# old = int(input('How old are you? '))
+# print(f'Мне {old * 2} лет')
+
+# old = 'asdasdasdsa'
+
+# # Хулиганство (он s присвоил функцию принт)
+# s = print
+# s('ddddd')
+
+# Программа 2
+a = input('Ваше имя? ')
+b = input('Ваше фамилия? ')
+c = input('Ваше отчество? ')
+
+# print(c, b, a, sep=':')
+# print(f'Привет, {b} {c} {a}')
+# print(f'Привет, {}')
+
+print(' '.join([c, b, a]), sep=':')
+print(' '.join([c, b, a])[::-1])
+print(f'{c+b}:{a}')
+
+# command+option+l = делает все по pip8
+# command + '?/' = #
+# Динамическая типизация + или - ?
+#
---
+++ Imports.py
@@ -0,0 +1,11 @@
+# import keyword
+# print(*keyword.kwlist)
+# # * убирает все лишнее
+
+x = float(input('Введите цифру: '))
+if x > 0:
+    print('Положительное!')
+elif x < 0:
+    print('Отрицательное!')
+else:
+    print('Равно 0!')
