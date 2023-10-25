b3533b8a8af7a111887c9649284382a6b909ea39
Сергей Комольцев <sk@Sergejs-MacBook-Air.local>
2023-10-25 16:38:30 +0300

    Corrected16

--- Homework16.py
+++ Homework16.py
@@ -2,53 +2,48 @@
 import re
 
 
-def create_abbreviation(string):
-    abbreviation = ''.join(re.findall(r'\b[A-Z]', string))
-    return abbreviation
+def generate_acronym(text):
+    words = re.findall(r'\b\w', text)
+    acronym = ''.join(words).upper()
+    return acronym
 
 
-abbreviation = create_abbreviation(input(str('Введите строку: ')))
-print(abbreviation)
+sentence = input(str('Введите строку: '))
+
+acronym = generate_acronym(sentence)
+print(acronym)
 
 
 # 16-2:
-arr1 = [-10, -1, 0, 1, 10, 5, 90, 20, 45, -17, 60, 30, 3, 75, 100]
+
+
+def find_positive_numbers(num, arr):
+    pattern = r'\b(?:[0-9]|[1-{}][0-9])\b(?![0-9])'.format(num, num)
+    matches = re.findall(pattern, ' '.join(map(str, arr)))
+    return list(map(int, matches))
+
+
 num = int(input('Введите положительное целое число: '))
+arr1 = [0, 1, 5, 10, 15, 99, 20, 25, 30, 45, 60, 1, 70, 80, 100]
+result = find_positive_numbers(num, arr1)
+result = [x for x in result if x != num]
+print(' '.join(map(str, result)))
 
 
-def find_numbers(num, arr):
-    array_str = ' '.join(map(str, arr))
-    result = re.sub(r'-\d+', '', array_str)
-
-    pattern = r'\b(?:0|[1-9]\d{0,1})\b'
-    matches = re.findall(pattern, result)
-
-    filtered_numbers = []
-    for match in matches:
-        if int(match) < num:
-            filtered_numbers.append(match)
-
-    return filtered_numbers
+# # 16-3:
+# def to_lowercase(func):
+#     def wrapper(*args, **kwargs):
+#         result = func(*args, **kwargs)
+#         if isinstance(result, str):
+#             return result.lower()
+#         return result
+#     return wrapper
 
 
-numbers = ' '.join(find_numbers(num, arr1))
-print(numbers)
+# @to_lowercase
+# def say():
+#     str = input('Напишите что-то сюда: ')
+#     return str
 
 
-# 16-3:
-def to_lowercase(func):
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        if isinstance(result, str):
-            return result.lower()
-        return result
-    return wrapper
-
-
-@to_lowercase
-def say():
-    str = input('Напишите что-то сюда: ')
-    return str
-
-
-print(say())
+# print(say())
